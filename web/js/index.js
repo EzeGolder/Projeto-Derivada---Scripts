0e7e0d0739100f03e73ddc910121f4dd4d68320f
kauandragues <kaua.rodrigues1509@gmail.com>
2025-10-14 21:19:49 -0300

    criação de interface web

---
+++ web/js/index.js
@@ -0,0 +1,113 @@
+let funcao = '';
+let funcoes = [];
+
+function Funcoes(funcao) {
+    let termos = [];
+    let termoAtual = '';
+    let coeficiente = 0;
+    let expoente = 0;
+
+    funcao = funcao.replace(/\s+/g, '');
+
+    if (funcao[0] !== '-' && funcao[0] !== '+')
+        funcao = '+' + funcao;
+
+    for (let i = 0; i < funcao.length; i++) {
+        const caractere = funcao[i];
+
+        if (caractere === 'x') {
+            if (termoAtual === '' || termoAtual === '+' || termoAtual === '-') {
+                coeficiente = termoAtual === '-' ? -1 : 1;
+            } else {
+                coeficiente = parseInt(termoAtual);
+            }
+
+            if (funcao[i + 1] === '^') {
+                let inicioExpoente = i + 2;
+                let fimExpoente = inicioExpoente;
+                while (fimExpoente < funcao.length && !isNaN(funcao[fimExpoente])) {
+                    fimExpoente++;
+                }
+                expoente = parseInt(funcao.slice(inicioExpoente, fimExpoente));
+                i = fimExpoente - 1;
+            } else {
+                expoente = 1;
+            }
+
+            termos.push({ coeficiente, expoente });
+            termoAtual = '';
+        } else if (caractere === '+' || caractere === '-') {
+            if (termoAtual !== '') {
+                if (termoAtual !== 'x' && termoAtual !== 'x^') {
+                    coeficiente = parseInt(termoAtual);
+                    expoente = 0;
+                    termos.push({ coeficiente, expoente });
+                }
+            }
+            termoAtual = caractere;
+        } else {
+            termoAtual += caractere;
+        }
+    }
+
+    return termos;
+}
+
+function derivada(coeficiente, expoente) {
+    if (expoente == 0) {
+        return [0, 0];
+    }
+
+    let novoCoeficiente = coeficiente * expoente;
+    let novoExpoente = expoente - 1;
+
+    return [novoCoeficiente, novoExpoente];
+}
+
+function calcularDerivada(funcoes) {
+    let derivadaFuncoes = [];
+
+    for (let i = 0; i < funcoes.length; i++) {
+        let { coeficiente, expoente } = funcoes[i];
+        let derivadaTermo = derivada(coeficiente, expoente);
+        derivadaFuncoes.push(derivadaTermo);
+    }
+
+    return derivadaFuncoes;
+}
+
+function exibirResultado(derivadaFuncoes) {
+    let resultado = "Derivada: ";
+
+    for (let i = 0; i < derivadaFuncoes.length; i++) {
+        let [coeficiente, expoente] = derivadaFuncoes[i];
+
+        if (coeficiente != 0) {
+            if (expoente == 0)
+                resultado += `${coeficiente}`;
+            else if (expoente == 1)
+                resultado += `${coeficiente}x^${expoente}`;
+            else
+                resultado += `${coeficiente}x^${expoente}`;
+
+            if (i < derivadaFuncoes.length - 1) {
+                resultado += " + ";
+            }
+        }
+    }
+
+    document.getElementById("resultado").innerText = resultado;
+}
+
+function mostrarResultado() {
+    funcao = document.getElementById("funcao").value;
+    funcoes = Funcoes(funcao);
+
+    console.log("\nFunção original:", funcao);
+
+    let derivadaFuncoes = calcularDerivada(funcoes);
+    exibirResultado(derivadaFuncoes);
+}
+
+const botao = document.getElementById("derivar");
+botao.addEventListener("click", mostrarResultado);
